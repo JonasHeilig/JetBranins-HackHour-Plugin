cc218b82f3ed6ec39898cd0026cae5ac78fcda78
Jonas Heilig <jonas.maximilian@die-heiligs.de>
2024-07-28 20:45:17 +0200

    Finish version 1 of the UI Window First Build This is my first Java/Kotlin Project

--- src/main/kotlin/de/jonasheilig/jetbraninshackhourplugin/HackHourToolWindowFactory.kt
+++ src/main/kotlin/de/jonasheilig/jetbraninshackhourplugin/HackHourToolWindowFactory.kt
@@ -1,7 +1,5 @@
 package de.jonasheilig.jetbraninshackhourplugin
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.ui.Messages
 import com.intellij.ide.util.PropertiesComponent
@@ -10,16 +8,17 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 import javax.swing.*
 import java.awt.BorderLayout
+import java.awt.CardLayout
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
-import java.net.URL
+import java.net.URI
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 
 class HackHourToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: com.intellij.openapi.project.Project, toolWindow: ToolWindow) {
         val panel = JPanel(BorderLayout())
-        val inputPanel = JPanel()
+        val inputPanel = JPanel(CardLayout())
         val displayPanel = JPanel()
 
         val slackIdLabel = JLabel("Slack ID:")
@@ -29,20 +28,44 @@ class HackHourToolWindowFactory : ToolWindowFactory {
         val apiKeyField = JTextField(20)
 
         val submitButton = JButton("Submit")
+        val toggleButton = JButton("Show Credentials")
 
-        val yourMinutesLabel = JLabel("Your Minutes:")
+        val yourMinutesLabel = JLabel("Your Sessions:")
         val yourMinutesValue = JLabel("0")
 
-        val yourHoursLabel = JLabel("Your Hours:")
+        val yourHoursLabel = JLabel("Your Minutes:")
         val yourHoursValue = JLabel("0")
 
         val updateButton = JButton("Update")
+        val debugButton = JButton("Show Raw Response")
 
         val properties = PropertiesComponent.getInstance()
         val savedSlackId = properties.getValue("hackhour.slackId", "")
         val savedApiKey = properties.getValue("hackhour.apiKey", "")
         slackIdField.text = savedSlackId
         apiKeyField.text = savedApiKey
+
+        var areCredentialsVisible = false
+
+        val credentialsPanel = JPanel()
+        credentialsPanel.layout = BoxLayout(credentialsPanel, BoxLayout.Y_AXIS)
+        credentialsPanel.add(slackIdLabel)
+        credentialsPanel.add(slackIdField)
+        credentialsPanel.add(apiKeyLabel)
+        credentialsPanel.add(apiKeyField)
+        credentialsPanel.add(submitButton)
+
+        inputPanel.add(JPanel(), "default")
+        inputPanel.add(credentialsPanel, "credentials")
+
+        val cardLayout = inputPanel.layout as CardLayout
+
+        toggleButton.addActionListener {
+            areCredentialsVisible = !areCredentialsVisible
+            cardLayout.show(inputPanel, if (areCredentialsVisible) "credentials" else "default")
+            panel.revalidate()
+            panel.repaint()
+        }
 
         submitButton.addActionListener {
             val slackId = slackIdField.text
@@ -62,6 +85,11 @@ class HackHourToolWindowFactory : ToolWindowFactory {
                     "Info",
                     Messages.getInformationIcon()
                 )
+                areCredentialsVisible = false
+                cardLayout.show(inputPanel, "default")
+                toggleButton.text = "Show Credentials"
+                panel.revalidate()
+                panel.repaint()
             }
         }
 
@@ -81,14 +109,16 @@ class HackHourToolWindowFactory : ToolWindowFactory {
             ApplicationManager.getApplication().executeOnPooledThread {
                 try {
                     val urlString = "https://hackhour.hackclub.com/api/stats/$slackId"
-                    val url = URL(urlString)
+                    val uri = URI(urlString)
+                    val url = uri.toURL()
                     val connection = url.openConnection() as HttpURLConnection
                     connection.requestMethod = "GET"
                     connection.setRequestProperty("Authorization", "Bearer $apiKey")
 
                     val responseCode = connection.responseCode
+                    val response = InputStreamReader(connection.inputStream).use { it.readText() }
+
                     if (responseCode == HttpURLConnection.HTTP_OK) {
-                        val response = InputStreamReader(connection.inputStream).use { it.readText() }
                         val json = Gson().fromJson(response, JsonObject::class.java)
                         val sessions = json["data"].asJsonObject["sessions"].asInt
                         val totalHours = json["data"].asJsonObject["total"].asInt
@@ -106,6 +136,16 @@ class HackHourToolWindowFactory : ToolWindowFactory {
                             )
                         }
                     }
+
+                    debugButton.addActionListener {
+                        SwingUtilities.invokeLater {
+                            Messages.showMessageDialog(
+                                response,
+                                "Raw Response",
+                                Messages.getInformationIcon()
+                            )
+                        }
+                    }
                 } catch (e: Exception) {
                     SwingUtilities.invokeLater {
                         Messages.showMessageDialog(
@@ -118,21 +158,19 @@ class HackHourToolWindowFactory : ToolWindowFactory {
             }
         }
 
-        inputPanel.layout = BoxLayout(inputPanel, BoxLayout.Y_AXIS)
-        inputPanel.add(slackIdLabel)
-        inputPanel.add(slackIdField)
-        inputPanel.add(apiKeyLabel)
-        inputPanel.add(apiKeyField)
-        inputPanel.add(submitButton)
+        val inputContainerPanel = JPanel(BorderLayout())
+        inputContainerPanel.add(toggleButton, BorderLayout.NORTH)
+        inputContainerPanel.add(inputPanel, BorderLayout.CENTER)
 
         displayPanel.layout = BoxLayout(displayPanel, BoxLayout.Y_AXIS)
         displayPanel.add(yourMinutesLabel)
-        displayPanel.add(yourHoursValue)
-        displayPanel.add(yourHoursLabel)
         displayPanel.add(yourMinutesValue)
+        displayPanel.add(yourHoursLabel)
+        displayPanel.add(yourHoursValue)
         displayPanel.add(updateButton)
+        displayPanel.add(debugButton)
 
-        panel.add(inputPanel, BorderLayout.NORTH)
+        panel.add(inputContainerPanel, BorderLayout.NORTH)
         panel.add(displayPanel, BorderLayout.CENTER)
 
         val contentFactory = ContentFactory.getInstance()
